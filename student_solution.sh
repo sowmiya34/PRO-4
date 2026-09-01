a8e98a0e927a1ed249adcfa9b253a6e7b268a570
Brundha S <brundharvs@gmail.com>
2026-09-01 11:21:15 +0530

    Update student_solution.sh

--- student_solution.sh
+++ student_solution.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
 
 # SELinux Access Denial Practical
-# Student Name:
-# Register Number:
+# Student Name:Brundha S
+# Register Number:1U24IT135
 
 echo "===== SELinux Status ====="
-
+getenforce
+sestatus
 
 echo "===== Creating Web Directory ====="
-
+sudo mkdir -p /var/www/html/selinux_practical
 echo "===== Creating HTML File ====="
-
+echo "<html><body><h1>SELinux Practical Test</h1></body></html>" | \
+sudo tee /var/www/html/selinux_practical/index.html > /dev/null
 
 echo "===== Setting Linux Permissions ====="
+sudo chmod 755 /var/www/html/selinux_practical
+sudo chmod 644 /var/www/html/selinux_practical/index.html
 
 
 echo "===== Checking Initial Context ====="
-
+ls -Zd /var/www/html/selinux_practical
+ls -Z /var/www/html/selinux_practical/index.html
 
 echo "===== Assigning Wrong SELinux Context ====="
+sudo chcon -t user_home_t /var/www/html/selinux_practical/index.html
 
 echo "===== Checking Wrong Context ====="
-
+ls -Z /var/www/html/selinux_practical/index.html
 
 echo "===== Checking AVC Denials ====="
-echo "===== Correcting SELinux Context ====="
+sudo ausearch -m AVC -ts recent
 
+echo "===== Correcting SELinux Context ====="
+sudo restorecon -v /var/www/html/selinux_practical/index.html
 
 echo "===== Checking Correct Context ====="
-
+ls -Z /var/www/html/selinux_practical/index.html
 
 echo "===== Practical Completed ====="
